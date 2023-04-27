c64bb36daa3e9ea6fac38c02c98a0402e740ffe3
Kyle Whaley <kylewhaley5@gmail.com>
2023-04-27 17:58:40 -0400

    Initial Commit

---
+++ SimpleDistortion/Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class SimpleDistortionAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    SimpleDistortionAudioProcessorEditor (SimpleDistortionAudioProcessor&);
+    ~SimpleDistortionAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    SimpleDistortionAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleDistortionAudioProcessorEditor)
+};
